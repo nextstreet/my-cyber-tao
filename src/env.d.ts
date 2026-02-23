85650afe63f7e82205ad85736419d94195c2f23f
nextstreet <dwq04@qq.com>
2026-02-23 23:46:51 +0800

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,8 +4,5 @@ declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
-<<<<<<< HEAD
 }
-=======
-  }
->>>>>>> 41ea5e06bc5f63a7a3f4bf725dcdc2139b948aed
+
