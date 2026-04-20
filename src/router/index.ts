4eaa117425d4810cbe0f19b4519235525877a7ea
nextstreet <dwq04@qq.com>
2026-04-20 12:23:11 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,21 @@ import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/about', name: 'about', component: () => import('../views/AboutView.vue') },
-    { path: '/destiny/:cardId', name: 'destiny', component: () => import('../views/DestinyView.vue') },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/result/:sessionId',
+      name: 'result',
+      component: () => import('../views/ResultView.vue'),
+    },
+    {
+      path: '/destiny',
+      name: 'destiny',
+      component: () => import('../views/DestinyView.vue'),
+    },
   ],
 })
 
