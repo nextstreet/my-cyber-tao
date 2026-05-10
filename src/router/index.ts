9ffd599232ed7d4dc3edd4a5d619d2abdce8c14e
nextstreet <dwq04@qq.com>
2026-05-10 17:13:05 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/about', name: 'about', component: () => import('../views/AboutView.vue') },
-    { path: '/destiny/:cardId', name: 'destiny', component: () => import('../views/DestinyView.vue') },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/cast',
+      name: 'cast',
+      component: () => import('@/views/CastView.vue'),
+    },
+    {
+      path: '/result/:id',
+      name: 'result',
+      component: () => import('@/views/ResultView.vue'),
+    },
+    {
+      path: '/destiny/:id',
+      name: 'destiny',
+      component: () => import('@/views/DestinyView.vue'),
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('@/views/AboutView.vue'),
+    },
+    // Catch-all → home
+    { path: '/:pathMatch(.*)*', redirect: '/' },
   ],
 })
 
